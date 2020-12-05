443567d1d165461bb5c5476009f69f450e8d5023
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2020-12-05 17:56:49 +0100

    breakpoint update

--- public/wp-content/themes/alex/functions.php
+++ public/wp-content/themes/alex/functions.php
@@ -2,8 +2,8 @@
 
 // enqueue scripts and styles
 function enqueue_scripts_and_styles() {
-    wp_enqueue_style('main-styles', get_template_directory_uri().'/main.65b23c5f51b9d00534ed.css?10a10ced29b932690dca', array(), null, false);
-    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.js?10a10ced29b932690dca', array(), null, false );
+    wp_enqueue_style('main-styles', get_template_directory_uri().'/main.b5ba2f6616f3c4a6dc65.css?6f2e430fc7a47fe3708c', array(), null, false);
+    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.js?6f2e430fc7a47fe3708c', array(), null, false );
 }
 
 // enable dynamic title tags
