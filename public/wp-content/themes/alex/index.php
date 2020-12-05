24e8b5a397bbfccbbdb3e04572a66158b65e83d7
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2020-12-05 17:48:52 +0100

    add news

--- public/wp-content/themes/alex/index.php
+++ public/wp-content/themes/alex/index.php
@@ -15,6 +15,7 @@
     }
 
     if(is_front_page()){
+        #leistungen
         $leistungen_query = new WP_Query("category_name=leistungen && posts_per_page=3");
         if($leistungen_query->have_posts()){
             echo "<section id='leistungen' class='leistungen'>";
@@ -22,8 +23,8 @@
             echo "<nav><ul>";
 
             while($leistungen_query->have_posts()){
-                echo "<li><a href='#'>";
                 $leistungen_query->the_post();
+                echo "<li><a href='#'>";
                 the_title("<p>", "</p>");
 
                 if(has_post_thumbnail()){
@@ -35,6 +36,34 @@
             echo "</ul></nav></section>";
         } else{
             echo "<p>keine Leistungen</p>";
+        }
+        
+        #news
+        $news_query = new WP_Query("category_name=news && posts_per_page=3");
+        if($news_query->have_posts()){
+            echo "<section id='news' class='news'>";
+            echo "<h2>News</h2>";
+            echo "<ul>";
+
+            while($news_query->have_posts()){
+                echo "<li><p>";
+                $news_query->the_post();
+
+                $exc = get_the_excerpt();
+                $marked = get_post_custom_values("marked");
+
+                foreach($marked as $mark){
+                    $exc = str_replace($mark, "<mark>" . $mark . "</mark>", $exc);
+                }
+                
+                echo $exc;
+                echo "<a href='". get_the_permalink() . "'>[mehr erfahren]</a>";
+                echo "</p></li>";
+            }
+
+            echo "</ul></section>";
+        } else{
+            echo "<p>keine News</p>";
         }
     }
     
