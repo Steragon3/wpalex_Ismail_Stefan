513594f2b63cebd05ff59838228b3f588d290e3e
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2020-12-05 22:17:36 +0100

    update contact

--- src/php/page-kontakt.php
+++ src/php/page-kontakt.php
@@ -1,15 +1,16 @@
 <?php 
     get_header();
-   
+    echo "<section class='contact'>";
     if(have_posts()){
         while(have_posts()){
             the_post();
+            the_title("<h2>", "</h2>");
             the_content();
         }
     } else{
-        echo "<p>Ich kann nicht kontaktiert werden.</p>";
+        echo "<p>Ich kann derzeit nicht kontaktiert werden.</p>";
     }
-
+    echo "</section>";
     wp_reset_postdata();
     get_footer();
 ?>
